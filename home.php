dfb7be600ce39250a3a6b041172684248701309b
Luis H <luisricardoholscher@gmail.com>
2024-05-01 21:12:50 -0300

    Cadastro de Clientes

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styleHome.css">
+    
+</head>
+<body>
+    <h1>Seja bem vindo!</h1>
+    <br>
+    <h1>Escolha uma opção abaixo</h1>
+    <div class="box">
+        <a href="login.php">Login</a>
+        <a href="formulario.php">Cadastre-se</a>
+
+    </div>
+</body>
+</html>
